fd316fa200aad0720e76d92003a9e146ed9132f6
Flora <flora.e.hewitt@gmail.com>
2020-06-27 21:39:26 +0400

    added fetchhelper functionality to handle errors and credentials

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Layout from './layout';
+import Layout from '@src/layout';
+import { handleErrors } from '@utils/fetchHelper';
 
 import './home.scss';
 
