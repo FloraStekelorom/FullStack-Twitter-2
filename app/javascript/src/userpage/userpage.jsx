942dd88fe9bd12b2b2565a82a5226442ae4b3d09
Flora <flora.e.hewitt@gmail.com>
2020-07-05 20:13:44 +0400

    added the userpage html and css - filtered tweets are missing

--- app/javascript/src/userpage/userpage.jsx
+++ app/javascript/src/userpage/userpage.jsx
@@ -1,15 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from '@src/common-component/layout';
+import Tweet from '@src/common-component/tweet';
+import ProfileCard from '@src/common-component/profileCard';
+import TweetForm from '@src/common-component/tweetForm';
+
+import { handleErrors } from '@utils/fetchHelper';
 
 import './userpage.scss';
 
 class Userpage extends React.Component {
+  state = {
+    tweets: [],
+  }
+
+  componentDidMount() {
+    this.getTweets();
+  }
+
+  getTweets = () => {
+    fetch('/api/tweets')
+      .then(handleErrors)
+      .then(data => {
+        this.setState({
+          tweets: data.tweets,
+        })
+      })
+  }
+  
   render () {
+    const { tweets } = this.state;
+
     return (
       <Layout>
-        <div className="container">
-          <h1>Userpage</h1>
+        <div id="feedPage">
+          <div className="row">
+            <div className="col-xs-3 profile-trends">
+              <ProfileCard />
+            </div>
+            <div className="col-xs-6 feed-box">
+              <TweetForm getTweets={this.getTweets} />
+              <div className="feed">
+                {tweets.length > 0 ? tweets.map((tweet) => {
+                  return (<Tweet
+                    key={tweet.id}
+                    tweet={tweet}
+                    getTweets={this.getTweets}
+                  />);
+                }) : <p>no tweets here</p>}
+
+              </div>
+            </div>
+            <div className="col-xs-3 follow-suggest">
+            </div>
+          </div>
         </div>
       </Layout>
     )
